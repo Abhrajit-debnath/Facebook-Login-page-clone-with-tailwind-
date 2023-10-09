0d8ab1c1a8c37c38b454b8e5d9cd692784315de5
Abhrajit <abhrajit>
2023-10-09 23:04:10 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      width:{
+        '98':'980px',
+        '321':'321px',
+        '580':'580px',
+        '396':'396px'
+      },
+      margin:{
+        '72':'72px',
+        '{-14}':'-14px'
+      },
+      borderWidth:{
+        1:'1px'
+      }
+    },
+  },
+  plugins: [],
+}
+
